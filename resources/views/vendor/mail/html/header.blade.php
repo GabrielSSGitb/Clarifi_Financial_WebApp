098884b692f3064188e7acb9ac70e3f452e3e4e5
GabrielSSGitb <gabrielsantosgithubac@gmail.com>
2026-05-18 09:39:29 -0300

    Feature: Added the recovery email sender

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ asset('build/assets/images/Clarifi_Oficial_Logo-removebg-preview.png') }}" class="logo" alt="{{ config('app.name') }}">
+</a>
+</td>
+</tr>
